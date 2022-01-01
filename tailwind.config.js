ea111c4004597eb1984422b197b1b1c7c60c79f9
Getachew Mulat <gech4me@gmail.com>
2022-01-01 22:48:10 +0300

    configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    extends: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    }
   },
   plugins: [],
 }
