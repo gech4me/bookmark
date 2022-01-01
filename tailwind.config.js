530ebb9910b9ba8c1c91a4a52531a5ccec403f7a
Getachew Mulat <gech4me@gmail.com>
2022-01-01 22:59:24 +0300

    colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extends: {},
+    extends: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      },
+    },
     fontFamily: {
       Poppins: ["Poppins, sans-serif"],
     },
